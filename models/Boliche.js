25f75febb3eb00e269e182c33a11fecf4a587665
Antonio3veces3 <cramirez28@ucol.mx>
2022-08-10 18:41:36 -0500

    Logic and rules finished
    Co-authored-by: AmarilloVel <hvelasco0@ucol.mx>

--- models/Boliche.js
+++ models/Boliche.js
@@ -1,92 +1,154 @@
 class Boliche {
   constructor() {
     this.bolos = 10;
-    this.turnos = 10;
     this.strike = false;
     this.spare = false;
-    this.bonus;
+    this.turnos = 10;
     this.total = 0;
     this.marcador = [
-      //Turno 1
-      //[1er tiro, 2do tiro, Puntaje]
-      //Turno 2
-      //[1er tiro, 2do tiro, Puntaje]
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        resultado: 0,
+      },
+      {
+        tiro1: 0,
+        tiro2: 0,
+        tiro3: 0,
+        resultado: 0,
+      },
     ];
     this.lanzamiento = [];
   }
-  primerTiro(result) {
-    if (this.strike) {
-      this.rulesStrike(result);
-    }
+  // primerTiro( marcador[0] )
+  primerTiro(turno) {
+    let lanzamiento = Math.floor(Math.random() * 10) + 1;
+    turno.tiro1 = lanzamiento;
+    this.bolos -= lanzamiento;
 
-    if (result == 10) {
-      this.lanzamiento = [10, 0, 10];
-      this.marcador.push(this.lanzamiento);
-      this.lanzamiento=[];
+    if (lanzamiento == 10) {
       this.strike = true;
+      this.bolos = 10;
     }
-
-    this.bolos -= result;
-
-    this.lanzamiento.push(result);
-    if (this.spare) this.rulesSpare(result);
   }
-  segundoTiro(result) {
-    result == this.bolos ? (this.spare = true) : (this.spare = false);
-    this.lanzamiento.push(result);
-    //Sumar puntaje Turno
-    let puntaje = this.lanzamiento[0] + this.lanzamiento[1];
-    this.lanzamiento.push(puntaje);
-    this.total += puntaje;
-    if (this.strike) this.rulesStrike(puntaje);
-    this.marcador.push(this.lanzamiento);
-    //this.actualizarMarcador();
-    this.lanzamiento = [];
+
+  segundoTiro(turno) {
+    let lanzamiento = Math.floor(Math.random() * this.bolos) + 1;
+    turno.tiro2 = lanzamiento;
+
+    if (lanzamiento == this.bolos) this.spare = true;
     this.bolos = 10;
-  }
-
-  rulesSpare(extra) {
-    let turnoActual = this.marcador.length;
-    this.marcador[turnoActual - 2] += extra;
-    this.total += extra;
-  }
-
-  rulesStrike(extra) {
-    let turnoActual = this.marcador.length;
-    this.marcador[turnoActual - 2] += extra;
-    this.total += extra;
-    this.strike = false;
-  }
-
-  actualizarMarcador() {
-    this.marcador.forEach((turno) => {
-      //console.log();
-    });
   }
 
   printScore() {
     console.log("Turno 1: ", this.marcador[0][2]);
     console.log("Turno 2: ", this.marcador[1][2]);
-    console.log(this.marcador)
+    console.log(this.marcador);
     console.log("Total: ", this.total);
   }
   juego() {
-    //Turno 1
-    //   this.primerTiro(6);
-    //   this.segundoTiro(4);
-    //Turno 2
-    //   this.primerTiro(1);
-    //   this.segundoTiro(4);
-    //Turno 3
-    this.primerTiro(10);
+    for (let i = 0; i < this.turnos; i++) {
+      this.primerTiro(this.marcador[i]);
+      // Fue un strike?
+      if (this.strike == true) {
+        this.marcador[i].resultado = 10;
+        // llamar a funcion para actualizar el marcador
+        this.total += this.marcador[i].resultado;
+        this.strike = false;
+        continue;
+      }
+      this.segundoTiro(this.marcador[i]);
 
-    //Turno 4
-    this.primerTiro(3);
-    this.segundoTiro(5);
-    //this.primerTiro(5);
+      this.marcador[i].resultado =
+        this.marcador[i].tiro1 + this.marcador[i].tiro2;
+      this.total += this.marcador[i].resultado;
+      // fue un spare?
+    }
+    console.table(this.marcador);
+    console.log(this.total);
+    //ACTUALIZAR EL MARCADOR CON LAS REGLAS
+    this.aplicarReglas();
+    console.log("------------------------");
+    console.table(this.marcador);
+    console.log(this.total);
+  }
 
-    //Imprimir
-    this.printScore();
+  aplicarReglas() {
+    this.total = 0;
+    for (let i = 0; i < this.marcador.length; i++) {
+      if (i != 9) {
+        // IS A STRIKE
+        if (this.marcador[i].tiro1 == 10) {
+          //spareReglas(i)
+
+          let bonus = this.marcador[i + 1].tiro1 + this.marcador[i + 1].tiro2;
+          this.marcador[i].resultado += bonus;
+        }
+
+        // IS A SPARE
+        if (this.marcador[i].tiro1 + this.marcador[i].tiro2 == 10) {
+          let bonus = this.marcador[i + 1].tiro1;
+          this.marcador[i].resultado += bonus;
+        }
+      }else{
+        //strike in last shot
+        if (this.marcador[i].tiro1 == 10) {
+          
+          this.marcador[i].tiro3 = Math.floor(Math.random() * 10) + 1;
+          let bonus = this.marcador[i].tiro3 + this.marcador[i].tiro1
+          this.marcador[i].resultado = bonus;
+
+          //spare in last shot
+        }else if(this.marcador[i].tiro1 + this.marcador[i].tiro2 == 10){
+
+          this.marcador[i].tiro3 = Math.floor(Math.random() * 10) + 1;
+          let bonus = this.marcador[i].tiro3 + this.marcador[i].tiro2 + this.marcador[i].tiro1;
+          this.marcador[i].resultado = bonus;
+        
+        }
+      }
+      this.total += this.marcador[i].resultado;
+    }
   }
 }
 
